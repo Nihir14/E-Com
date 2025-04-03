23fb33edb8b13c6c158df7a0f7284389d1e34e8b
Nihir14 <nihirverma14@gmail.com>
2025-04-03 11:27:44 +0530

    update
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/nihir/e_com_pro/EComProApplication.java
@@ -0,0 +1,13 @@
+package com.nihir.e_com_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nihir/e_com_pro/EComProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nihir.e_com_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
